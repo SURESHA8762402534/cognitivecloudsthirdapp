1ad9e64cca2f45c0b86e9aee5e06929f85e8d9ae
suresh <sureshamahendra.gmail.com>
2022-04-06 15:08:10 +0530

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import FrontPage from './components/FrontPage';
-import Newpage from './components/Newpage';
 import RawJSON from './components/RawJSON';
-import FourOFour from './components/FourOFour';
+
 
 
 function App() {
   return (
     <>
     <FrontPage/>
-    {/* <Newpage/> */}
+   
       <Routes>
       <Route path="/jsonData" element={<RawJSON/>}/>
-        {/* <Route path='/' element={<FourOFour/>}/> */}
+       
       </Routes>
     </>
   );
