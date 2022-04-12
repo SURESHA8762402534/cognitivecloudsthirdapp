937814f24acbc71e49ef16ab3612321a112939b5
suresh <sureshamahendra.gmail.com>
2022-04-12 16:16:27 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,61 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import FrontPage from './components/FrontPage';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import RawJSON from './components/RawJSON';
+import { useEffect, useState } from 'react';
+import PostsTable from './components/PostsTable';
 
+const App: React.FC = () => {
+  const [page, setPage] = useState(1);
+  const [storedData, setStoredData]: any = useState({});
+  const navigate = useNavigate();
+  let interval: NodeJS.Timer;
 
-
-function App() {
-  return (
-    <>
+  const fetchTableData = async () => {
+    const response = await fetch(
+      `https://hn.algolia.com/api/v1/search_by_date?query=story&page=${page}`
+    );
+    const resData = await response.json();
     
-   
+  console.log(resData)
+    if (storedData[page] === undefined) {
+      setStoredData({ ...storedData, [page]: resData.hits });
+    }
+  };
+
+
+  const handelPage = ()=>{
+    setPage((page)=>page+1)
+    console.log('page called');
+  }
+
+  useEffect(() => {
+    fetchTableData();
+  }, [page]);
+
+  useEffect(()=>{
+    interval = setInterval(handelPage,10000)
+  },[])
+
+  return (
+    <div className="app">
       <Routes>
-      <Route path="/" element={<FrontPage/>}/>
-      <Route path='/jsondata' element={<RawJSON/>}/>
-       
+        
+        <Route
+          path="/"
+          element={
+            <PostsTable
+              storedData={storedData}
+              page={page}
+              setPage={setPage}
+              navigate={navigate}
+            />
+          }
+        />
+         <Route path='/jsondata' element={<RawJSON/>}/>
       </Routes>
-    </>
+    </div>
   );
-}
+};
+
 
 export default App;
